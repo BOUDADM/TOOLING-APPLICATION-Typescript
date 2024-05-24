fd9481221a2e5e5a9f294c3c05759cb002f44d54
BOUDADM <161730952+BOUDADM@users.noreply.github.com>
2024-05-24 14:32:30 +0100

    Update main.ts

--- src/DATASERIAL/main.ts
+++ src/DATASERIAL/main.ts
@@ -1,9 +1,6 @@
-// main.ts
-
 import { jsonToByteArray } from './DATASERIAL';
 const prompt = require('prompt-sync')();
 
-// Function to get user input and process it
 function getUserInputAndProcess() {
     const jsonString = prompt('Please enter a JSON string: ');
 
@@ -16,14 +13,11 @@ function getUserInputAndProcess() {
         return;
     }
 
-    // Convert JSON object to string and byte array
     const jsonStringOutput: string = JSON.stringify(jsonObject);
     const byteArray: string = jsonToByteArray(jsonObject);
 
-    // Display the results
     console.log('JSON String: ', jsonStringOutput);
     console.log('Byte Array: ', byteArray);
 }
 
-// Run the function
 getUserInputAndProcess();
