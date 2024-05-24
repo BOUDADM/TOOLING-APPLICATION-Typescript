d59b6aaac2b52a225fb2fe3ffdc4bad09804499a
BOUDADM <161730952+BOUDADM@users.noreply.github.com>
2024-05-24 14:31:05 +0100

    Update DATADESRIAL.ts

--- src/DATASERIAL/DATADESRIAL.ts
+++ src/DATASERIAL/DATADESRIAL.ts
@@ -1,14 +1,9 @@
-// datadeserial.ts
-
 export {};
 
-// Function to convert byte array to JSON string
 export function byteArrayToJson(byteArray: number[]): string {
     let jsonString: string = '';
     
-    // Iterate through each byte in the array
     for (let i = 0; i < byteArray.length; i++) {
-        // Convert the byte into a character and append it to the string
         jsonString += String.fromCharCode(byteArray[i]);
     }
     
